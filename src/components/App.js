cd413cbe90b0a01dfb0981d476bf1c2f9150bec0
Cherise Foster <cherise.foster@outlook.com>
2024-01-06 12:34:48 -0700

    working on navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './NavBar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+      <NavBar />
+      <Routes>
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </Routes>
+    </Router>
   );
 }
 
