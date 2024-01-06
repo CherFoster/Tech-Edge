02c597ccb46b73a52c5cd379ff7745dd2c0c4af4
Cherise Foster <cherise.foster@outlook.com>
2024-01-06 14:05:56 -0700

    navbar done for now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 
 function NavBar(){
     return(
-        <nav className='header'>
-            <div className='nav-items'>
-                <Link className='nav-link' to='/home'>Home</Link>
-                <Link className='nav-link' to='/about'>About</Link>
-                <Link className='nav-link' to='/portfolio'>Portfolio</Link>
-                <Link className='nav-link' to='contact'>Contact</Link>
-            </div>
-        </nav>
+        <header className='header'>
+            <nav className='header'>
+                <div className='nav-items'>
+                    <Link className='nav-link' to='/home'>Home</Link>
+                    <Link className='nav-link' to='/about'>About</Link>
+                    <Link className='nav-link' to='/portfolio'>Portfolio</Link>
+                    <Link className='nav-link' to='contact'>Contact</Link>
+                </div>
+            </nav>
+        </header>
     )
 }
 
