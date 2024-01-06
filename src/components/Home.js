dfb6074554767b61c324499632fdf7616a4c0084
Cherise Foster <cherise.foster@outlook.com>
2024-01-06 15:23:55 -0700

    changed styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -51,15 +51,15 @@ function Home(){
     //     `;
     //     document.head.appendChild(style);
 
-    return(
-        <div className="home-page">
-            <img alt ='logo' className="logo" src='https://i.postimg.cc/qMp0wdd4/Tech-Edge-removebg-preview-removebg-preview.png' />
-            <h1 className="home-h1">Where Code Creates Impact</h1>
-            <p className="home-p">At Tech Edge, innovation meets reliability in the realm of developing. I am a passionate software engineer, dedicated to transforming your digital dreams into reality.</p>
-
-            <div id="code-container"></div>
+    return (
+        <div className="home-page hero"> {/* Added 'hero' class */}
+          <img alt='logo' className="logo" src='https://i.postimg.cc/qMp0wdd4/Tech-Edge-removebg-preview-removebg-preview.png' />
+          <h1 className="home-h1">Where Code Creates Impact</h1>
+          <p className="home-p">At Tech Edge, innovation meets reliability in the realm of developing. I am a passionate software engineer, dedicated to transforming your digital dreams into reality.</p>
+    
+          <div id="code-container"></div>
         </div>
-    )
-}
+      );
+    }
 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function NavBar(){
-    return(
-        <header className='header'>
-            <nav className='header'>
-                <div className='nav-items'>
-                    <Link className='nav-link' to='/home'>Home</Link>
-                    <Link className='nav-link' to='/about'>About</Link>
-                    <Link className='nav-link' to='/portfolio'>Portfolio</Link>
-                    <Link className='nav-link' to='contact'>Contact</Link>
-                </div>
-            </nav>
-        </header>
-    )
+function NavBar() {
+  return (
+    <header className='header'>
+        <nav className="nav"> {/* Assuming 'nav' class from your CSS */}
+        <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/portfolio'>Portfolio</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
+        </ul>
+        </nav>
+    </header>
+  );
 }
 
 export default NavBar;
