8a07b03ac3a93f62f3470df0a99737399e89f106
Cherise Foster <cherise.foster@outlook.com>
2024-01-07 10:58:34 -0700

    added arrow

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,55 +1,10 @@
 import React from "react";
 
 function Home(){
-    // const colors = ['#D4D4D4', '#569CD6', '#4EC9B0', '#C586C0', '#9CDCFE', '#CE9178', '#F44747'];
-    // const maxLines = 30; // Maximum number of lines on screen
-    // const lineDuration = 5000; // Duration of each line's animation in milliseconds
-    // const newLineInterval = 200; // Interval for creating new lines in milliseconds
-    // let linesOnScreen = 0;
-
-    // function generateRandomCode() {
-    //     const characters = '01';
-    //     let result = '';
-    //     const charactersLength = characters.length;
-    //     for (let i = 0; i < 30; i++) {
-    //         result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    //     }
-    //     return result;
-    //     }
-
-    //     function createCodeLine() {
-    //         if (linesOnScreen >= maxLines) {
-    //             return; 
-    //     }
-
-    //     const codeLine = document.createElement('div');
-    //     codeLine.textContent = generateRandomCode();
-    //     codeLine.style.position = 'absolute';
-    //     codeLine.style.left = `${Math.random() * 100}%`;
-    //     codeLine.style.top = '0';
-    //     codeLine.style.color = colors[Math.floor(Math.random() * colors.length)];
-    //     codeLine.style.transform = `translateX(-50%)`;
-    //     codeLine.style.animation = `moveCode ${lineDuration / 1000}s linear`;
-
-    //     document.getElementById('code-container').appendChild(codeLine);
-    //     linesOnScreen++; 
-
-    //     setTimeout(() => {
-    //         codeLine.remove();
-    //         linesOnScreen--; 
-    //     }, lineDuration);
-    //     }
-
-    //     setInterval(createCodeLine, newLineInterval);
-
-    //     const style = document.createElement('style');
-    //     style.innerHTML = `
-    //     @keyframes moveCode {
-    //         from { top: -20px; }
-    //         to { top: 100%; }
-    //     }
-    //     `;
-    //     document.head.appendChild(style);
+   
+    function scrollDown(){
+        document.getElementById('services').scrollIntoView({behavior: 'smooth'});
+    }
 
     return (
         <div className="home-page hero">
@@ -59,8 +14,13 @@ function Home(){
             </div>
           <h1 className="home-h1">Where Code Creates Impact</h1>
           <p className="home-p">At Tech Edge, innovation meets reliability in the realm of developing. I am a passionate software engineer, dedicated to transforming your digital dreams into reality.</p>
-    
-          <div id="code-container"></div>
+          <p className="home-p2">Check out our services</p>
+          <div class="container">
+                <a data-scroll href="#services">
+                 <div class="arrow"></div>
+                </a>
+            </div>
+            <div id="full"></div>
         </div>
       );
     }
